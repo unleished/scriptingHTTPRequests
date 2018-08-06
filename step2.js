4573cc065e547c0deffc697b8372e6fef1352f9b
Leisha Magsisi <leisha.gallagher@gmail.com>
2018-08-06 17:16:47 +0000

    updating step 2 & 3 to move variable to last call & move empty variable up a scope.

--- step2.js
+++ step2.js
@@ -2,7 +2,7 @@ var https = require('https');
 
 
 function getAndPrintHTML () {
-
+var fullData = '';
   var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step2.html'
@@ -15,16 +15,15 @@ function getAndPrintHTML () {
 
   // the callback is invoked when a `data` chunk is received
   response.on('data', function (data) {
-    var fullData = '';
     fullData += data;
-    console.log('Chunk Received. Full Data:', fullData);
+    console.log('Chunk Received.');
 
   });
 
   // the callback is invoked when all of the data has been received
   // (the `end` of the stream)
   response.on('end', function() {
-    console.log('this is all the data');
+    console.log('this is all the data', fullData);
   });
 
 });
--- step3.js
+++ step3.js
@@ -1,24 +1,24 @@
 var https = require('https');
 
 function getAndPrintHTML (options) {
+var fullOptions = '';
 
   /* Add your code here */
-  https.get(requestOptions, function (response) {
+  https.get(options, function (response) {
 
     // set encoding of received data to UTF-8
     response.setEncoding('utf8');
 
     // the callback is invoked when a `data` chunk is received
     response.on('data', function (options) {
-      var fullOptions = '';
       fullOptions += options;
-      console.log('options:', options);
+      console.log('gathering options');
     });
 
     // the callback is invoked when all of the data has been received
     // (the `end` of the stream)
     response.on('end', function() {
-      console.log('this is all the data');
+      console.log('this is all the data', fullOptions);
     });
   });
 }
@@ -28,4 +28,4 @@ var requestOptions = {
   path: '/http-examples/step3.html'
 };
 
-getAndPrintHTML();
+getAndPrintHTML(requestOptions);
